9abbb087c611680015568e6aaec1f4081881b383
stand-1 <79456201+stand-1@users.noreply.github.com>
2021-12-21 17:06:56 +0600

    first commit

---
+++ src/Componetns/Modal/modal.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Modal.css';
+
+
+const Modal = (active, setActive) => {
+    return (
+        <>
+            <div className={active ? "modal active" : 'modal'} onClick={() => setActive(false)}>
+            <div className='modal-content' onClick={e => e.stopPropagation()}>
+
+            </div>
+        </div>
+            </>
+    );
+};
+
+export default Modal;
---
+++ src/Componetns/title/title.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './title.css';
+
+const Title = () => {
+    return (
+        <div>
+            <h1 className='subtitle'>
+                Over 2000 businesses, marketers and creatives use Suparise to grow their Instagram.
+            </h1>
+            <h2 className='title'>
+                Don’t just take our word for it
+            </h2>
+        </div>
+    );
+};
+
+export default Title;
